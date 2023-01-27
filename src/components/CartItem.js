eeb1a51cb45476c29a55fba3072de93210857c17
Krishan Kumar <kkrishan7074@gmail.com>
2023-01-27 21:32:08 +0530

    Commit

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,15 +1,27 @@
+import React, { useState } from "react";
+
 function CartItem() {
+
+    const [quantity, setQuantity] = useState(0);
+    const [data] = useState({ price: 999, title: "Mobile Phone" });
+
+    const increaseCount = () => {
+        setQuantity(quantity + 1);
+    }
+
     return (
         <div className="cart-item">
             <div className="left-block">
                 <img style={styles.image} alt="" />
             </div>
             <div className="right-block">
-                <div style={{ fontSize: 40 }}>Phone</div>
-                <div style={{ color: "#777" }}>Rs 999</div>
-                <div style={{ color: "#777" }}>Qty: 1</div>
+                <div style={{ fontSize: 45 }}>{data.title}</div>
+                <div style={{ fontSize: 20, color: "#777" }}>Rs {data.price}</div>
+                <div style={{ fontSize: 20, color: "#777" }}>Qty: {quantity}</div>
                 <div className="cart-item-actions">
-                    {/*Buttons*/}
+                    <img src="https://cdn-icons-png.flaticon.com/512/992/992651.png" alt="increase" className="action-items" onClick={increaseCount} />
+                    <img src="https://cdn-icons-png.flaticon.com/512/1828/1828906.png" alt="decrease" className="action-items" />
+                    <img src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" alt="delete" className="action-items" />
                 </div>
             </div>
         </div>
@@ -18,9 +30,9 @@ function CartItem() {
 
 const styles = {
     image: {
-        height: 250,
-        width: 250,
-        borderRadius: 4,
+        height: 180,
+        width: 180,
+        borderRadius: 10,
         backgroundColor: "#ccc",
     }
 }
