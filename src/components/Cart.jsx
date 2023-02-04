ea5af71666c55be7c7d003439878d42eabd2f478
Krishan Kumar <kkrishan7074@gmail.com>
2023-02-05 00:03:57 +0530

    Commit

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,17 +1,20 @@
-import React, { useState } from 'react';
-import CartItem from './CartItem';
+import React, { useState } from "react";
+import CartItem from "./CartItem";
 
 function Cart() {
-    const [data] = useState([{ id: 1, price: 999, title: "Mobile Phone" }, { id: 2, price: 99, title: "Watch" }, { id: 3, price: 599, title: "MacBook" }]);
+  const [data] = useState([
+    { id: 1, price: 999, title: "Mobile Phone" },
+    { id: 2, price: 99, title: "Watch" },
+    { id: 3, price: 599, title: "MacBook" },
+  ]);
 
-    return (
-        <div id="cart">
-            {data.map((item) => {
-                return <CartItem price={item.price} title={item.title} key={item.id} />
-            })}
-        </div>
-    )
-
+  return (
+    <div id="cart">
+      {data.map((item) => {
+        return <CartItem price={item.price} title={item.title} key={item.id} />;
+      })}
+    </div>
+  );
 }
 
 export default Cart;
--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,49 +1,61 @@
 import React, { useState } from "react";
 
 function CartItem(props) {
+  const [quantity, setQuantity] = useState(1);
 
-    const [quantity, setQuantity] = useState(1);
+  const increaseCount = () => {
+    setQuantity(quantity + 1);
+  };
 
-    const increaseCount = () => {
-        setQuantity(quantity + 1);
+  const decreaseCount = () => {
+    if (quantity === 0) {
+      return;
     }
+    setQuantity(quantity - 1);
+  };
 
-    const decreaseCount = () => {
-        if (quantity === 0) {
-            return;
-        }
-        setQuantity(quantity - 1);
-    }
-
-    console.log("Props:", props);
-    const { price, title } = props;
-    return (
-        <div className="cart-item">
-            <div className="left-block">
-                <img style={styles.image} alt="" />
-            </div>
-            <div className="right-block">
-                <div style={{ fontSize: 45 }}>{title}</div>
-                <div style={{ fontSize: 20, color: "#777" }}>Rs {price}</div>
-                <div style={{ fontSize: 20, color: "#777" }}>Qty: {quantity}</div>
-                <div className="cart-item-actions">
-                    <img src="https://cdn-icons-png.flaticon.com/512/992/992651.png" alt="increase" className="action-items" onClick={increaseCount} />
-                    <img src="https://cdn-icons-png.flaticon.com/512/1828/1828906.png" alt="decrease" className="action-items" onClick={decreaseCount} />
-                    <img src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" alt="delete" className="action-items" />
-                </div>
-            </div>
+  console.log("Props:", props);
+  const { price, title } = props;
+  return (
+    <div className="cart-item">
+      <div className="left-block">
+        <img style={styles.image} alt="" />
+      </div>
+      <div className="right-block">
+        <div style={{ fontSize: 45 }}>{title}</div>
+        <div style={{ fontSize: 20, color: "#777" }}>Rs {price}</div>
+        <div style={{ fontSize: 20, color: "#777" }}>Qty: {quantity}</div>
+        <div className="cart-item-actions">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+            alt="increase"
+            className="action-items"
+            onClick={increaseCount}
+          />
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/1828/1828906.png"
+            alt="decrease"
+            className="action-items"
+            onClick={decreaseCount}
+          />
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
+            alt="delete"
+            className="action-items"
+          />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 const styles = {
-    image: {
-        height: 180,
-        width: 180,
-        borderRadius: 10,
-        backgroundColor: "#ccc",
-    }
-}
-
+  image: {
+    height: 180,
+    width: 180,
+    borderRadius: 10,
+    backgroundColor: "#ccc",
+  },
+};
 
 export default CartItem;
