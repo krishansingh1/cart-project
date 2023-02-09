4dceeeec3b7db243d5105823467d44e6585ff6c1
Krishan Kumar <kkrishan7074@gmail.com>
2023-02-09 17:55:45 +0530

    Commite

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Cart from "./components/Cart";
 import CartClass from "./components/CartClass";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <CartClass />
       {/* <Cart /> */}
     </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+function Navbar() {
+  return (
+    <div style={styles.nav}>
+      <div style={styles.cartIconContainer}>
+        <img
+          style={styles.cartIcon}
+          src="https://cdn-icons-png.flaticon.com/512/3144/3144456.png"
+          alt="Cart-Image"
+        />
+        <span style={styles.cartCount}>3</span>
+      </div>
+    </div>
+  );
+}
+
+const styles = {
+  cartIcon: {
+    height: 50,
+    marginRight: 20,
+    marginTop: 5,
+  },
+  nav: {
+    height: 70,
+    background: "#4267b2",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  cartIconContainer: {
+    positon: "relative",
+  },
+  cartCount: {
+    background: "yellow",
+    border: "1px solid red",
+    borderRadius: "50%",
+    position: "absolute",
+    padding: "2px 8px",
+    top: 5,
+    right: 5,
+  },
+};
+
+export default Navbar;
